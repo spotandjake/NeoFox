5def6c8502c5e74fc0d0eaff007bb7dda0bcb9a2
Spotandjake <spotandjake@hotmail.com>
2024-06-10 20:48:23 -0400

    feat(admin): Deploy to github pages

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ module.exports = withBundleAnalyzer({
   env: {
     Storage_Url: 'https://storage.googleapis.com/united-rope-234818.appspot.com'
   },
+  output: "export",
   images: {
+    // You can re-enable this if you are not using a static export
+    unoptimized: true,
     domains: [
       'pbs.twimg.com',
       'lh3.googleusercontent.com',
